0fc874db30535967f19484bf919e306ab141885d
Striatp <sebastien.garcia@mail.com>
2026-04-25 18:18:55 +0200

    Update install.sh
    Signed-off-by: Striatp <sebastien.garcia@mail.com>

--- forgecli/install.sh
+++ forgecli/install.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 ORG="striatp"
 REPO="striatp"
 
-printf "\033[90m[%s]\033[0m \033[34m[INFO]\033[0m %s" "$(date +%H:%M:%S)" "The installation process has started.\n"
+printf "\033[90m[%s]\033[0m \033[34m[INFO]\033[0m %s\n" "$(date +%H:%M:%S)" "The installation process has started."
 
 # ----- Helpers ----- #
 
